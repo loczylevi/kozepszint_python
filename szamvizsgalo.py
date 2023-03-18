9b41fe61aaecd0dbcb0e2174efbd9370d8e1ca4e
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-03-18 14:06:16 +0100

    Update szamvizsgalo.py

--- szamvizsgalo.py
+++ szamvizsgalo.py
@@ -15,10 +15,29 @@ A szám nem osztható öttel és tízzel.\n""")
 print(f"""3. feladat
 A szám visszafelé olvasva: {bekeres[::-1]}\n""")
       
-
-print(f"""4. feladat
-A páros számjegyek növekvő sorrendben: [2, 4, 4, 6]""")
     
 paros_szamok = []
 for szam in bekeres:
-    if int(szam)
+    if int(szam) % 2 == 0:
+        paros_szamok.append(int(szam))
+
+if len(paros_szamok) > 0:
+    print(f"""4. feladat
+A páros számjegyek növekvő sorrendben: {sorted(paros_szamok)}""")
+else:
+    print('A számban nincsenek páros számjegyek.')
+
+
+ismetlodo_szamjegyek = []
+for szamjegy in bekeres:
+    if bekeres.count(szamjegy) > 1 and szamjegy not in ismetlodo_szamjegyek:
+        ismetlodo_szamjegyek.append(szamjegy)
+
+if len(ismetlodo_szamjegyek) > 0:
+    print(f"""5. feladat
+Az ismétlődő számjegyek: {' ,'.join(sorted(ismetlodo_szamjegyek))}""")
+else:
+    print(f"""5. feladat
+Nincsenek ismétlődő számjegyek: """)
+
+# 6 os passz nem értem
